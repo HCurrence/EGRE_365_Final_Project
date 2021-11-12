ca2febdfe1a68b1c44685b72c38e0e512b82b969
HCurrence <46232898+HCurrence@users.noreply.github.com>
2021-11-12 15:25:35 -0500

    SPI_Control Skeleton Done

--- final_project.srcs/sources_1/new/SPI_Control.vhd
+++ final_project.srcs/sources_1/new/SPI_Control.vhd
@@ -11,7 +11,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity SPI_Control is
---  Port ( );
+  port ( start : in std_logic;                              -- clock_divider
+         reset : in std_logic;                              -- reset
+         tx_end : in std_logic;                             -- o_tx_end
+         o_data_parallel: in std_logic_vector(15 downto 0); -- o_data_parallel
+         clk : out std_logic;                               -- i_clk
+         rstb : out std_logic;                              -- i_rstb?
+         tx_start : out std_logic;                          -- i_tx_start
+         i_data_parallel : out std_logic_vector(15 downto 0); --i_data_parallel
+         xaxis_data : out std_logic_vector(15 downto 0);    -- x data out
+         yaxis_data : out std_logic_vector(15 downto 0);    -- y data out
+         zaxis_data : out std_logic_vector(15 downto 0));   -- z data out
 end SPI_Control;
 
 architecture Behavioral of SPI_Control is
