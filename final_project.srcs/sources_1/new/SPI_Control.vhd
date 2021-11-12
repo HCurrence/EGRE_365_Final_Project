ac50ef1b5b310b909bbb540f01cc64df9c7ee42f
HCurrence <46232898+HCurrence@users.noreply.github.com>
2021-11-12 15:13:15 -0500

    Added original spi_controller and testbench.

---
+++ final_project.srcs/sources_1/new/SPI_Control.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity SPI_Control is
+--  Port ( );
+end SPI_Control;
+
+architecture Behavioral of SPI_Control is
+
+begin
+
+
+end Behavioral;
